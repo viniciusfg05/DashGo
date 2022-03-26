4d3b173590f7a515a24b20359d07c16549933fae
unknown <viniciusserlimpa@gmail.com>
2022-03-26 11:02:49 -0300

    Iniciando a componetização da SideBar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1fe76788985fb6c1"; }
+/******/ 		__webpack_require__.h = function() { return "22fafc31b4f6aee8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,15 @@
+import { Flex } from "@chakra-ui/react";
 import { Header } from "../components/Header";
+import SideBar from "../components/Sidebar";
 
 export default function Dashboard() {
   return (
-    <Header />
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth={1080} mx="auto" px="6">
+        <SideBar/>
+      </Flex>
+    </Flex>
   )
 }
